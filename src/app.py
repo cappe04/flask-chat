247d992e5243e07ac2927254ea99ab5881976ac3
Casper Bené <48201144+cappe04@users.noreply.github.com>
2023-08-18 22:43:50 +0200

    ¯\_(ツ)_/¯

--- src/app.py
+++ src/app.py
@@ -1,16 +1,17 @@
 import functools
-import os
-import datetime
+import time
 from typing import Optional
 
 from flask import Flask, render_template, request, redirect, url_for, make_response
 from flask_socketio import SocketIO, join_room
-import jwt
+from pydantic import ValidationError
 
-import cookies
+from . import settings
+from . import cookies
+from .schemas import ChatMessage
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("SECREY_KEY", default="")
+app.config["SECRET_KEY"] = settings.SECRET_KEY
 socketio = SocketIO(app)
 
 db = {
@@ -27,17 +28,21 @@ def run(**kwargs):
     socketio.run(app, **kwargs)
 
 
-def require_cookie(*requirements: str, redirect_to: Optional[str] = None):
+def require_cookie(*requirements: str, 
+                   redirect_to: Optional[str] = None, 
+                   with_args: bool = True):
     def require_auth_inner(f):
         endpoint = f.__name__ if redirect_to is None else redirect_to
 
         @functools.wraps(f)
         def wrapper(*args, **kwargs):
 
+            endpoint_args = kwargs if with_args else {}
+
             for requirement in requirements:
                 cookie = request.cookies.get(requirement)
-                if cookie is None or not cookies.validate_cookie(cookie, app.secret_key):
-                    return redirect(url_for("login", redirect_to=endpoint, **kwargs))
+                if cookie is None or not cookies.validate_cookie(cookie):
+                    return redirect(url_for("login", redirect_to=endpoint, **endpoint_args))
             
             return f(*args, **kwargs)
         return wrapper
@@ -45,31 +50,32 @@ def require_cookie(*requirements: str, redirect_to: Optional[str] = None):
 
 
 @app.route("/")
-def root():
+def index():
     return redirect(url_for("home"))
 
 
 @app.route("/home/")
-@require_cookie("token", "user")
+@require_cookie("token", "user_id")
 def home():
-    if (user := cookies.get_cookie_from(request.cookies, "user", app.secret_key)) is None: 
+    if (user := cookies.get_cookie_from(request.cookies, "user_id")) is None: 
         return
     
     return render_template("home.html", 
-                           user=user, 
+                           username=user.get("user_id"), 
                            channel_items=channel.items())
 
 
 @app.route("/chat/<int:channel_id>")
-@require_cookie("token", "user", redirect_to="home")
+@require_cookie("token", "user_id")
 def chat(*, channel_id):
-    if (user := cookies.get_cookie_from(request.cookies, "user", app.secret_key)) is None: 
-        return
     if (channel_name := channel.get(channel_id)) is None:
         return
-  
-    return render_template("chat.html", 
-                           user=user, 
+    
+    user = cookies.get_cookie_from(request.cookies, "user_id")
+    user_id = user.get("user_id")
+
+    return render_template("chat.html",
+                           username=user_id,
                            channel_name=channel_name)
 
 
@@ -86,11 +92,11 @@ def login():
             endpoint = args.pop("redirect_to", "home")
             response = make_response(redirect(url_for(endpoint, **args)))
 
-            token_cookie = cookies.new_cookie(app.secret_key)
+            token_cookie = cookies.new_cookie()
             response.set_cookie("token", token_cookie)
-            
-            user_cookie = cookies.new_cookie(app.secret_key, payload={ "username": username })
-            response.set_cookie("user", user_cookie)
+
+            user_cookie = cookies.new_cookie(payload={ "user_id": username })
+            response.set_cookie("user_id", user_cookie)
 
             return response
         
@@ -102,23 +108,24 @@ def login():
 
 
 
-@socketio.on("user_connect")
-def event_user_connect(data):
-    if (user := cookies.get_cookie_from(request.cookies, "user", app.secret_key)) is None: 
-        return
-
+@socketio.on("client_connect")
+def client_connect(data):
     if (channel_id := data.get("channel_id")) is None:
         return
-
-    join_room(channel_id)
+    
+    join_room(int(channel_id))
 
 
 @socketio.on("client_send_message")
 def send_message(data):
-    if (user := cookies.get_cookie_from(request.cookies, "user", app.secret_key)) is None: 
+    user = cookies.get_cookie_from(request.cookies, "user_id")
+    timestamp = int(time.time())
+    try:
+        message = ChatMessage(user_id=user.get("user_id"), 
+                              timestamp=timestamp, 
+                              **data)
+    except ValidationError:
+        socketio.emit("refresh")
         return
 
-    if (channel_id := data.get("channel_id")) is None:
-        return
-
-    socketio.emit("server_send_message", data | user, to=channel_id)
+    socketio.emit("server_send_message", message.model_dump_json(), to=message.channel_id)
