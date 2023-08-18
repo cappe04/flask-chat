77a46580d48bc83b8f589b499ad5f507e790f3ea
Casper Bené <48201144+cappe04@users.noreply.github.com>
2023-08-18 22:43:24 +0200

    default key and cookie lifetime from dotenv

--- src/cookies.py
+++ src/cookies.py
@@ -1,40 +1,40 @@
 import datetime
-from typing import Optional, Any
+from typing import Any
 
 import jwt
 
-import time
+from . import settings
 
-DEFAULT_LIFETIME = datetime.timedelta(seconds=5)
 ALGORITHM = "HS256"
 
-def new_cookie(secret: str | bytes, 
-               exp: datetime.timedelta = DEFAULT_LIFETIME,
+def new_cookie(secret: str | bytes = settings.SECRET_KEY, 
+               exp: datetime.timedelta = settings.COOKIE_LIFETIME,
                payload: dict[str, Any] = {}) -> str:
     
     _payload = payload | { "exp": datetime.datetime.utcnow() + exp }
     return jwt.encode(_payload, secret, algorithm=ALGORITHM)
 
-def validate_cookie(cookie: str | bytes, secret: str | bytes) -> bool:
+def validate_cookie(cookie: str | bytes, secret: str | bytes = settings.SECRET_KEY) -> bool:
     try:
         # thorws error if exp has passed
         jwt.decode(cookie, secret, algorithms=[ALGORITHM])
         return True
-    except jwt.ExpiredSignatureError:
+    except (jwt.ExpiredSignatureError, jwt.InvalidSignatureError):
         return False
 
-def get_cookie(cookie: str, secret: str | bytes) -> Any | None:
+def get_cookie(cookie: str, secret: str | bytes = settings.SECRET_KEY) -> dict:
     try:
         # thorws error if exp has passed
-        return jwt.decode(cookie, secret, algorithms=[ALGORITHM])
-    # catch expired signature
-    except jwt.ExpiredSignatureError:
-        return None
-    # catch decode error
-    except jwt.exceptions.DecodeError:
-        return None
-    
-def get_cookie_from(source: dict[str, str], dict_key: str, secret: str | bytes) -> Any | None:
+        payload = jwt.decode(cookie, secret, algorithms=[ALGORITHM])
+        payload.pop("exp")
+        return payload
+    # catch expired signature and decode error
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return {}
+
+def get_cookie_from(source: dict[str, str], 
+                    dict_key: str, 
+                    secret: str | bytes = settings.SECRET_KEY) -> dict:
     if (cookie := source.get(dict_key)) is None:
-        return None
+        return {}
     return get_cookie(cookie, secret)
