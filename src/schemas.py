a93d63b2325f51d032de5a46fccff579915a4a1b
Casper Bené <48201144+cappe04@users.noreply.github.com>
2023-08-19 14:40:03 +0200

    ChatMessage inherit DatabaseMessage

--- src/schemas.py
+++ src/schemas.py
@@ -1,21 +1,8 @@
-from pydantic import BaseModel, FieldValidationInfo, field_validator, ValidationError
+from datetime import datetime
+
+from pydantic import BaseModel, field_validator, ValidationError
 
 MAX_MESSAGE_LENGTH = 256 # maybe in .env
-
-class ChatMessage(BaseModel):
-    username: str
-    channel_id: int
-    message: str
-    timestamp: int
-    message_id: int
-
-    @field_validator("username", "message") # not really needed for username but why not...
-    @classmethod
-    def cap_string_length(cls, v: str) -> str:
-        if len(v) > MAX_MESSAGE_LENGTH:
-            return v[:MAX_MESSAGE_LENGTH]
-        return v
-
 
 class DatabaseMessage(BaseModel):
     channel_id: int
@@ -25,7 +12,18 @@ class DatabaseMessage(BaseModel):
 
     @field_validator("message")
     @classmethod
-    def cap_string_length(cls, v: str):
+    def cap_message_length(cls, v: str):
         if len(v) > MAX_MESSAGE_LENGTH:
             return v[:MAX_MESSAGE_LENGTH]
         return v
+
+class ChatMessage(DatabaseMessage):
+    username: str
+    message_id: int
+    timestamp: str | int
+
+    @field_validator("timestamp")
+    @classmethod
+    def convert_timestamp(cls, v: int) -> str:
+        return datetime.fromtimestamp(v).strftime('%Y-%m-%d %H:%M:%S')
+
