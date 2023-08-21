33f8353340dc4e4045ca0c797873abb3bd1e392c
Casper Bené <48201144+cappe04@users.noreply.github.com>
2023-08-21 23:08:38 +0200

    load messages with socket

--- app/socketio.py
+++ app/socketio.py
@@ -6,7 +6,7 @@ from flask_socketio import SocketIO, join_room
 
 from app import cookies
 from app.schemas import convert_timestamp
-from app.db import DbHandle
+from app.db import DbHandle, MessageFormat
 
 
 socketio = SocketIO()
@@ -15,7 +15,7 @@ init_app = socketio.init_app
 run = socketio.run
 
 
-@socketio.on("client_connect")
+@socketio.on("client:connect")
 def client_connect(data):
     channel_id = data.get("channel_id")
     if not DbHandle().validate_channel(channel_id):
@@ -23,7 +23,7 @@ def client_connect(data):
 
     join_room(channel_id)
 
-@socketio.on("client_send_message")
+@socketio.on("message:send")
 def send_message(data):
     user_id = cookies.get_cookie_from(request.cookies, "user_id").get("user_id")
 
@@ -33,11 +33,23 @@ def send_message(data):
     channel_id = data["channel_id"]
 
     if message_id is None or not DbHandle().validate_channel(channel_id):
-        socketio.emit("refresh")
+        socketio.emit("page:reload")
         return
     
-    socketio.emit("server_send_message", json.dumps({
+    socketio.emit("message:new", data | {
         "message_id": message_id,
         "username": DbHandle().get_username(user_id),
         "timestamp": convert_timestamp(timestamp)
-    } | data), to=channel_id)
+    }, to=channel_id)
+
+@socketio.on("message:get")
+def load_messages(data):
+    if (offset := data.get("offset")) is None:
+        return
+    if (channel_id := data.get("channel_id")) is None:
+        return
+    
+    messages = DbHandle().get_messages(channel_id, 
+                                       offset=offset, 
+                                       format=MessageFormat.CHAT)
+    socketio.emit("message:load", messages, to=request.sid)
--- app/static/js/chat.js
+++ app/static/js/chat.js
@@ -1,37 +1,62 @@
+const channel_id = parseInt(document.location.pathname.slice(-1))
+let offset = 0
+
+function render_message(message, insert=false){
+    offset++;
+    html = `<div class=message data-id=${message.message_id}>
+    <strong>${message.username}</strong>: ${message.message} (<i>${message.timestamp}</i>)
+    </div>`
+
+    if(insert)
+        $("#chat").prepend(html)
+    else
+        $("#chat").append(html)
+}
+
 $("document").ready(function(){
     socket = io.connect(document.location.origin)
 
-    // socket.on("connect", function(){
-    //     socket.emit("user_connect")
-    // })
-    
-    const channel_id = parseInt(document.location.pathname.slice(-1))
-
     socket.on("connect", function(){
-        socket.emit("client_connect", {
+        socket.emit("client:connect", {
             "channel_id": channel_id
+        })
+        socket.emit("message:get", {
+            "channel_id": channel_id,
+            "offset": offset
         })
     })
 
-    socket.on("refresh", function(){
+    socket.on("page:reload", function(){
         console.log("Something went wrong, reloading page!")
         location.reload()
     })
 
-    socket.on("server_send_message", function(data){
-        var message = JSON.parse(data)
-        $("#chat").append(
-            `<div class=message data-id=${message.message_id}>
-            <strong>${message.username}</strong>: ${message.message} (<i>${message.timestamp}</i>)
-            </div>`
-        )
-        console.log(data)
+    socket.on("message:new", function(message){
+        render_message(message)
+        console.log(message)
+    })
+
+    socket.on("message:load", function(messages){
+        if (messages == undefined){
+            console.log("There are no more messages to load!")
+            return
+        }
+        for(message of messages){
+            render_message(message, true)
+        }
     })
 
     $("#btn_send").click(function(){
-        socket.emit("client_send_message", {
+        socket.emit("message:send", {
             "message": $("#input_message").val(),
             "channel_id": channel_id
+        })
+    })
+
+    $("#btn_load").click(function(){
+        socket.emit("message:get", {
+            "channel_id": channel_id,
+            "offset": offset
         })
     })
 
