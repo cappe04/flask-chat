181ef5cc8954efb616a9d81b4ff41ffb42556c42
Casper Bené <48201144+cappe04@users.noreply.github.com>
2023-08-23 00:18:58 +0200

    Frontend :(

--- app/static/js/chat.js
+++ app/static/js/chat.js
@@ -1,57 +1,101 @@
-let offset = 0
+class Chat {
+    constructor(socket){
+        this.socket = socket
+        
+        this.chat = $("#chat")
+        this.inputMessage = $("#input_message")
+        this.buttonSend = $("#btn_send")
+        this.buttonLoad = $("#btn_load")
 
-function render_message(message, insert=false){
-    offset++;
-    html = `<div class=message data-id=${message.message_id}>
-    <strong>${message.username}</strong>: ${message.message} (<i>${message.timestamp}</i>)
-    </div>`
+        this.loadIndicator = $('<div class="loading">Loading more messages...</div>')
+        this.emptyIndicator = $('<div class="loading">No more messages...</div>')
 
-    if(insert)
-        $("#chat").prepend(html)
-    else
-        $("#chat").append(html)
+        this.offset = 0
+
+        // Idk wtf this is but ChatGPT told me to do it
+        this.onNew = this.onNew.bind(this);
+        this.onLoad = this.onLoad.bind(this);
+        this.send = this.send.bind(this);
+        this.get = this.get.bind(this);
+        this.scrollHandler = this.scrollHandler.bind(this);
+
+        this.socket.on("message:new", this.onNew)
+        this.socket.on("message:load", this.onLoad)
+
+        this.chat.on("scroll", this.scrollHandler)
+        this.buttonSend.click(this.send)
+        this.buttonLoad.click(this.get)
+
+        this.chat.prepend(this.loadIndicator)
+    }
+    
+    onNew(message){
+        var html = this.renderMessage(message)
+        this.chat.append(html)
+        this.offset++
+        var height = this.chat.prop("scrollHeight")
+        this.chat.scrollTop(height)
+    }
+
+    onLoad(messages){
+        this.loadIndicator.remove()
+        if(messages == undefined){
+            this.chat.prepend(this.emptyIndicator)
+            return
+        }
+        var preLoadHeight = this.chat.prop("scrollHeight")
+        for(var message of messages){
+            var html = this.renderMessage(message)
+            this.chat.prepend(html)
+            this.offset++
+        }
+        var postLoadHeight = this.chat.prop("scrollHeight")
+        this.chat.scrollTop(postLoadHeight- preLoadHeight)
+    }
+
+    send(){
+        this.socket.emit("message:send", {
+            "message": this.inputMessage.val()
+        })
+        this.inputMessage.val("")
+    }
+
+    get(){
+        this.socket.emit("message:get", {
+            "offset": this.offset
+        })
+    }
+
+    scrollHandler(){
+        this.chat.prepend(this.loadIndicator)
+        if(this.chat.scrollTop() === 0){
+            this.get()
+        }
+    }
+
+    renderMessage(message){
+        var html = `
+        <div class=message data-id=${message.message_id}>
+            <strong>${message.username}</strong>: ${message.message} (<i>${message.timestamp}</i>)
+        </div>
+        `
+        return html
+    }
 }
 
 $("document").ready(function(){
     socket = io.connect(document.location.origin)
 
+    const chat = new Chat(socket)
+
     socket.on("connect", function(){
-        socket.emit("message:get", {
-            "offset": offset
-        })
+        chat.get()
     })
+
 
     socket.on("page:reload", function(){
         console.log("Something went wrong, reloading page!")
         location.reload()
-    })
-
-    socket.on("message:new", function(message){
-        render_message(message)
-        console.log(message)
-    })
-
-    socket.on("message:load", function(messages){
-        if (messages == undefined){
-            console.log("There are no more messages to load!")
-            return
-        }
-        for(message of messages){
-            render_message(message, true)
-        }
-    })
-
-    $("#btn_send").click(function(){
-        socket.emit("message:send", {
-            "message": $("#input_message").val(),
-        })
-        $("#input_message").val("")
-    })
-
-    $("#btn_load").click(function(){
-        socket.emit("message:get", {
-            "offset": offset
-        })
     })
 
     $("#btn_home").click(function(){
